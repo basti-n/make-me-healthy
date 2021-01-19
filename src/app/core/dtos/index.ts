c2683e9a359690c3b2c36834842afa4a26a30581
Sebastian <sebastian.neumair@gmail.com>
2021-01-19 21:29:54 +0100

    Update to Angular 7

---
+++ src/app/core/dtos/index.ts
@@ -0,0 +1,4 @@
+// start:ng42.barrel
+export * from './stackexchange.search.model';
+// end:ng42.barrel
+
--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -1,40 +1,46 @@
-import {Injectable} from "@angular/core";
-import {Http, Response} from "@angular/http";
-import {Observable} from "rxjs";
+import { catchError, tap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StackexchangeSearchDTO } from '../dtos';
 
-export interface ISearchResultItem  {
-    answer_count: number;
-    closed_date: number;
-    closed_reason: string;
-    creation_date: number;
-    is_answered: boolean;
-    last_activity_date: number;
-    link: string;
-    score: number;
-    tags: Array<string>;
-    title: string;
-    view_count: number;
+export interface ISearchResultItem {
+  answer_count: number;
+  closed_date: number;
+  closed_reason: string;
+  creation_date: number;
+  is_answered: boolean;
+  last_activity_date: number;
+  link: string;
+  score: number;
+  tags: Array<string>;
+  title: string;
+  view_count: number;
 }
 
 @Injectable()
 export class SearchService {
+  private static readonly apiUrl =
+    'https://api.stackexchange.com/2.2/search?pagesize=20&order=desc&sort=activity&site=stackoverflow&intitle=';
 
-    private static readonly apiUrl =
-        "https://api.stackexchange.com/2.2/search?pagesize=20&order=desc&sort=activity&site=stackoverflow&intitle=";
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: Http) {
-
-    }
-
-    search(keyword: string): Observable<JSON> {
-        return this.http.get(SearchService.apiUrl + keyword)
-            .map((res: Response) => {
-                let data = res.json();
-                console.log("API USAGE: " + data.quota_remaining + " of " + data.quota_max + " requests available" );
-                return data;
-            })
-            .catch((err: Response) => Observable.of(err.json()));
-    }
-
-
+  search(keyword: string): Observable<StackexchangeSearchDTO> {
+    return this.http
+      .get<StackexchangeSearchDTO>(SearchService.apiUrl + keyword)
+      .pipe(
+        tap(
+          (data) =>
+            console.log(
+              'API USAGE: ' +
+                data.quota_remaining +
+                ' of ' +
+                data.quota_max +
+                ' requests available'
+            ),
+          catchError((err: Error) => of(err))
+        )
+      );
+  }
 }
