6c1b66ad57edc44b19b5c154af1364c8c4b5e5ae
Sebastian <sebastian.neumair@gmail.com>
2021-01-20 22:28:00 +0100

    Add ClientId Interceptor for Search Requests

---
+++ src/app/core/interceptors/client-id.interceptor.spec.ts
@@ -0,0 +1,63 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { SearchService } from '../services';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ClientIdInterceptor } from './client-id.interceptor';
+import { take } from 'rxjs/operators';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MockOtherService {
+  API_URL = 'http://unit.test/user';
+  constructor(private http: HttpClient) {}
+
+  search(): Observable<unknown> {
+    return this.http.get<unknown>(this.API_URL);
+  }
+}
+
+describe('ClientIdInterceptor', () => {
+  let httpMock: HttpTestingController;
+  let searchService: SearchService;
+  let otherService: { search: () => Observable<unknown> };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        SearchService,
+        MockOtherService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: ClientIdInterceptor,
+          multi: true,
+        },
+      ],
+    });
+
+    searchService = TestBed.inject(SearchService);
+    httpMock = TestBed.inject(HttpTestingController);
+    otherService = TestBed.inject(MockOtherService);
+  });
+  it('should the clientId for search requests', async () => {
+    const queryString = 'randomSearchQuery';
+    searchService.search(queryString).pipe(take(1)).subscribe();
+
+    const request = httpMock.expectOne(
+      `${SearchService['apiUrl']}${queryString}&key=${environment.clientId}`
+    );
+    expect(request.request.params.get('key')).toBeTruthy();
+  });
+
+  it('should not add a clientId param for search requests', () => {
+    otherService.search().subscribe();
+
+    const request = httpMock.expectOne('http://unit.test/user');
+    expect(request.request.params.get('key')).toBeFalsy();
+  });
+});
