231b244757f3a2f2b595d98a3f01f094bfd58d8f
Sebastian <sebastian.neumair@gmail.com>
2021-01-22 07:11:03 +0100

    Add cache to reduce number of redundant http requests

--- src/app/core/models/base/search-service-provider.interface.ts
+++ src/app/core/models/base/search-service-provider.interface.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 
 export abstract class SearchService<SearchResult> {
   private static readonly BASE_URL: string;
+  protected readonly cache: Map<string, SearchResult>;
 
   abstract search(queryString: string): Observable<SearchResult>;
 }
--- src/app/core/services/search/stackexchange.service.ts
+++ src/app/core/services/search/stackexchange.service.ts
@@ -5,10 +5,13 @@ import { Observable, of } from 'rxjs';
 import { StackexchangeSearchDTO } from '../../models/dtos';
 import { SearchService } from 'app/core/models/base';
 
+const STACKEXCHANGE_URL =
+  'https://api.stackexchange.com/2.2/search?pagesize=20&order=desc&sort=activity&site=stackoverflow&intitle=';
+
 @Injectable({ providedIn: 'root' })
 export class StackExchangeSearchService extends SearchService<StackexchangeSearchDTO> {
-  private static readonly apiUrl =
-    'https://api.stackexchange.com/2.2/search?pagesize=20&order=desc&sort=activity&site=stackoverflow&intitle=';
+  private static readonly apiUrl = STACKEXCHANGE_URL;
+  protected readonly cache = new Map<string, StackexchangeSearchDTO>();
 
   constructor(private http: HttpClient) {
     super();
@@ -20,20 +23,42 @@ export class StackExchangeSearchService extends SearchService<StackexchangeSearc
   }
 
   search(keyword: string): Observable<StackexchangeSearchDTO> {
-    return this.http
-      .get<StackexchangeSearchDTO>(StackExchangeSearchService.apiUrl + keyword)
-      .pipe(
-        tap(
-          (data) =>
-            console.log(
-              'API USAGE: ' +
-                data.quota_remaining +
-                ' of ' +
-                data.quota_max +
-                ' requests available'
-            ),
-          catchError((err: Error) => of(err))
-        )
-      );
+    if (this.isCached(keyword)) {
+      return of(this.getFromFromCache(keyword));
+    }
+
+    return this.http.get<StackexchangeSearchDTO>(this.getUrl(keyword)).pipe(
+      tap((result) => {
+        this.saveToCache(keyword, result);
+        this.logUsage(result);
+      }),
+      catchError((err: Error) => of({ items: [] } as StackexchangeSearchDTO))
+    );
+  }
+
+  private logUsage(result: StackexchangeSearchDTO): void {
+    console.log(
+      'API USAGE: ' +
+        result.quota_remaining +
+        ' of ' +
+        result.quota_max +
+        ' requests available'
+    );
+  }
+
+  private saveToCache(keyword: string, result: StackexchangeSearchDTO): void {
+    this.cache.set(keyword, result);
+  }
+
+  private isCached(keyword: string): boolean {
+    return this.cache.has(keyword);
+  }
+
+  private getFromFromCache(keyword: string): StackexchangeSearchDTO | null {
+    return this.cache.get(keyword);
+  }
+
+  private getUrl(keyword: string): string {
+    return StackExchangeSearchService.apiUrl + encodeURIComponent(keyword);
   }
 }
