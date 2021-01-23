123738f8a9e7e3755cc4218e067857fa174ac4e3
Sebastian <sebastian.neumair@gmail.com>
2021-01-23 22:14:08 +0100

    Add Test for Dashboard Widget Navigation Items

--- src/app/pages/dashboard/dashboard.component.spec.ts
+++ src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppRoutingModule } from 'app/app.routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard.routing.module';
@@ -19,11 +21,27 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show a nav link to all widgets from the config', async () => {
+    const navigationLinks: DebugElement[] = fixture.debugElement.queryAll(
+      By.css('nav > li')
+    );
+    expect(navigationLinks.length).toEqual(
+      Object.keys(component.widgetConfigs).length
+    );
+
+    const nativeElements = navigationLinks.map((debug) => debug.nativeElement);
+
+    expect(
+      nativeElements.find(
+        (el: HTMLElement) => el.textContent === component.widgetConfigs[0].label
+      )
+    );
   });
 });
