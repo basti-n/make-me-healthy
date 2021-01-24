153688d742c1843c830b950ae8142160774b87d9
Sebastian <sebastian.neumair@gmail.com>
2021-01-24 13:57:58 +0100

    Add Widget Heading and Bullet Unit Tests

--- src/app/components/widget/widget-item/widget-bullets/widget-bullets.component.spec.ts
+++ src/app/components/widget/widget-item/widget-bullets/widget-bullets.component.spec.ts
@@ -1,24 +1,54 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { WidgetItem } from 'app/core/models/definitions';
 import { WidgetBulletsComponent } from './widget-bullets.component';
+
+const mockBullets: WidgetItem['bullets'] = [
+  {
+    value: 'Unit',
+    label: 'Test',
+  },
+  { value: 'Red', label: 'Medical' },
+  { value: 'Stay', label: 'Healthy' },
+];
 
 describe('WidgetBulletsComponent', () => {
   let component: WidgetBulletsComponent;
   let fixture: ComponentFixture<WidgetBulletsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ WidgetBulletsComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [WidgetBulletsComponent],
+      }).compileComponents();
     })
-    .compileComponents();
-  });
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WidgetBulletsComponent);
     component = fixture.componentInstance;
+    component.bullets = mockBullets;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render label and value for each bullet', () => {
+    const bullets: HTMLDListElement[] = fixture.debugElement
+      .queryAll(By.css('li'))
+      .map((debugElement) => debugElement.nativeElement);
+
+    expect(
+      component.bullets.every((bullet) =>
+        bullets.find(
+          (li) =>
+            li.textContent.includes(String(bullet.value)) &&
+            li.textContent.includes(bullet.label)
+        )
+      )
+    ).toBeTruthy();
   });
 });
