5c4b8b21f61300496a45f78570b6eab15b6693f7
Sebastian <sebastian.neumair@gmail.com>
2021-01-24 09:53:40 +0100

    Add Weather Service Test

--- src/app/core/services/search/weather.service.spec.ts
+++ src/app/core/services/search/weather.service.spec.ts
@@ -1,1 +1,21 @@
-describe('WeatherServuce', () => {});
+import { take } from 'rxjs/operators';
+import { WeatherSearchService } from './weather.service';
+
+describe('WeatherService', () => {
+  let service: WeatherSearchService;
+
+  beforeEach(() => {
+    service = new WeatherSearchService();
+  });
+
+  it('should be created', () => {
+    expect(service instanceof WeatherSearchService).toBeTruthy();
+  });
+
+  describe('search', () => {
+    it('should return the weatherdata', async () => {
+      const result = await service.search().pipe(take(1)).toPromise();
+      expect(result.days.length).toBeGreaterThan(1);
+    });
+  });
+});
