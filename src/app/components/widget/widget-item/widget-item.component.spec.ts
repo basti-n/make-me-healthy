ac23842f0b7ea18bcc758de3a1267186476741ae
Sebastian <sebastian.neumair@gmail.com>
2021-01-24 10:38:50 +0100

    Add Widget Item Component Test

--- src/app/components/widget/widget-item/widget-item.component.spec.ts
+++ src/app/components/widget/widget-item/widget-item.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { WidgetModule } from '../widget.module';
+import { WidgetBulletsComponent } from './widget-bullets/widget-bullets.component';
+import { WidgetItemHeadingComponent } from './widget-item-heading';
 import { WidgetItemComponent } from './widget-item.component';
 
 describe('WidgetItemComponent', () => {
@@ -8,9 +11,7 @@ describe('WidgetItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        WidgetModule,
-      ],
+      imports: [WidgetModule],
     }).compileComponents();
   });
 
@@ -20,7 +21,20 @@ describe('WidgetItemComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the heading', () => {
+    const heading = fixture.debugElement.query(
+      By.directive(WidgetItemHeadingComponent)
+    );
+    expect(heading).toBeTruthy();
+  });
+  it('should render the bullets', () => {
+    const bullets = fixture.debugElement.query(
+      By.directive(WidgetBulletsComponent)
+    );
+    expect(bullets).toBeTruthy();
   });
 });
